1a7ea72eff841b65372400d3236fd9c9c1a1963a
CHENNAKRISHNAN <64550690+chennakrishnans@users.noreply.github.com>
2022-09-29 13:37:09 +0530

    Update first fit.c

--- first fit.c
+++ first fit.c
@@ -1,84 +1,65 @@
-#include<stdio.h>
-#include<conio.h>
-int main( ) 
+#include <stdio.h>
+
+void implimentFirstFit(int blockSize[], int blocks, int processSize[], int processes)
 {
-int i , j, temp, b[ 10] , c[ 10] , arr , n, ch, a;  
-printf("\t\t FIRST FIT, BEST FIT, WORST FIT\n"); 
-printf( "Enter the size of no .of blocks:") ; 
-scanf ( "%d" , &n) ; 
-for ( i =1; i <=n; i ++) 
-{ 
-printf ( "Enter the size of %d block: " , i ) ; 
-scanf ("%d" , &b[ i ] ) ; 
-c[ i ] =b[ i ] ; 
-} 
-printf ( "\n Enter the size of Arriving block:" ) ;
-scanf ("%d" , & arr ) ; 
-printf ( "\n1. First fit \n2. Best fit \n3. Worst fit \n Enter your choice: " ) ; 
-scanf ( "%d" , &ch) ; 
-switch( ch) 
-{ 
-case1: 
-for ( i =1; i <=n; i ++) 
-{ 
-if ( b[ i ] >=arr ) 
-{ 
-printf ( " Arriving block is allocated to % d block. " , i ) ; 
-break; 
-} 
-else 
-continue; 
-} 
-break; 
-case2: 
-for( i =1; i <=n; i ++) 
-{ 
-for ( j =1; j <n ; j ++) 
-{ 
-if ( b[ i ] >=b[ i +1] ) 
-{ 
-temp=b[ i ] ; 
-b[ i ] =b[ i +1] ; 
-b[ i +1] =temp; 
+   
+    int allocate[processes];
+    
+    
+    for(int i = 0; i < processes; i++)
+    {
+		allocate[i] = -1;
+    }
+	
+    
+    for (int i = 0; i < processes; i++)
+    {
+        for (int j = 0; j < blocks; j++) { 
+            if (blockSize[j] >= processSize[i])
+            {
+                
+                allocate[i] = j;
+ 
+               
+                blockSize[j] -= processSize[i];
+ 
+                break;
+            }
+        }
+    }
+
+    printf("\nProcess No.\tProcess Size\tBlock no.\n");
+    for (int i = 0; i < processes; i++)
+    {
+        printf("%d \t\t\t %d \t\t\t", i+1, processSize[i]);
+        if (allocate[i] != -1)
+            printf("%d\n",allocate[i] + 1);
+        else
+            printf("Not Allocated\n");
+    }
 }
-} 
-} 
-for( i =1; i <=n; i ++) 
-{ 
-if ( b[ i ] >=arr ) 
-{ 
-a=b[ i ] ; 
-break; 
-} 
-else 
-continue; 
-} 
-for ( i =1; i <=n; i ++) 
-{ 
-if ( c[ i ] ==a) 
-{ 
-printf ( "Arriving block is allocated to %d block." , i ) ; 
-} 
-} 
-break; 
-case3: 
-for ( i =1; i <=n; i ++) 
-{ 
-for ( j =1; j <n; j ++) 
-{ 
-if ( b[ i ] >=b[ i +1] ) 
-{ 
-temp=b[ i ] ;
-b[ i ] =b[ i +1] ; 
-b[ i +1] =temp; 
-} 
-} 
-} 
-for ( i =1; i <=n; i ++) 
-printf ( "%d" , b[ i ] ) ; 
-break; 
-default :
-printf ( "Enter the valid choice: " ) ; 
-} 
-getch( ) ; 
+
+int main()
+{
+    int blockSize[100];
+    int processSize[100];
+    
+    int m,i;
+    int n;
+    printf("Enter the size of blocks:");
+    scanf("%d",&m);
+    for(i=0;i<m;i++)
+    {
+    	printf("Enter the block %d:",i+1);
+    	scanf("%d",&blockSize[i]);
+	}
+	printf("Enter the size of arriving blocks:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+    	printf("Enter the block arriving %d:",i+1);
+    	scanf("%d",&processSize[i]);
+	}
+    implimentFirstFit(blockSize, m, processSize, n);
+    return 0;
 }
